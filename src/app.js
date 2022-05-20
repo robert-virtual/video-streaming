aee98dc6602e6bcb63c252c1a087b4ffcaeca8da
robert-virtual <robertocastill63@gmail.com>
2022-05-20 14:40:35 -0600

    better

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static("public"));
 app.get("/video", (req, res) => {
-  let stream = fs.createReadStream("videos/video.mp4");
+  let { range } = req.headers;
+  if (!range) {
+    return res.status(400).send("Requires Range header");
+  }
+
+  let videoSize = fs.statSync("videos/video.mp4").size;
+  // parse range
+  // ejemplo: "Bytes=32324-"
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, "")); // remover todos los caracteres que no son numeros o digitos
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+  res.writeHead(206, headers);
+
+  let stream = fs.createReadStream("videos/video.mp4", { start, end });
   stream.pipe(res);
 });
 app.listen(port, () => {
